f380256035e65d3f6b1ca097897e2a9597bfe056
Sam Doshi <sam@metal-fish.co.uk>
2016-03-21 20:19:20 +0000

    ii.h is common to all modules, move it to skeleton

--- whitewhale/config.mk
+++ whitewhale/config.mk
@@ -113,7 +113,6 @@ INC_PATH = \
        ../skeleton/usb/ftdi \
        ../skeleton/usb/hid \
        ../skeleton/usb/midi \
-       ../teletype \
        avr32/boards                                       \
        avr32/drivers/cpu/cycle_counter                    \
        avr32/drivers/flashc                               \
